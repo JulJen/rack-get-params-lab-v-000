828529e791b7bf5f6b21b9c0cf0604adfe1da3b5
Jul Jen <juliajen.prog@gmail.com>
2018-03-26 21:52:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,6 @@ class Application
     elsif req.path.math(/cart/)
       if !@@cart.empty?
         @@cart.each { |item| resp.write "#{item}\n" }
-      end 
       else
         resp.write "Your cart is empty"
       end
